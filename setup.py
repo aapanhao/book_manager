6417fe1385c6d923a30467bb6a138e850db82751
panhao <aapanhao@163.com>
2023-05-12 09:53:54 +0800

    fix

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ import setuptools  # type: ignore
 setuptools.setup(
     name="book_manager",
     version="1.0.0",
-    packages=setuptools.find_packages(), )
+    packages=setuptools.find_packages(),
+    package_data={
+        'NAME': ['*.pyi'],
+    },
+)
